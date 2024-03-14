100c0ab362a391f8e419c76975c67184de178ff2
lbrs <62109485+cdrclbrs@users.noreply.github.com>
2024-03-14 15:41:37 +0100

    Update rshellduck.ps1

--- rshellduck.ps1
+++ rshellduck.ps1
@@ -22,12 +22,12 @@ Write-Host 'Ne laissez pas votre ordinateur non verouillé!
 ███████▄░░░░░░░░░░░▄███████
 ██████████▄▄▄▄▄▄▄██████████
 ███████████████████████████
-
 1- Verrouillage de l ordinateur : Sécurisez l accès avec un mot de passe ou une authentification biométrique
 2- VPN : Chiffrez votre connexion avec un VPN
 3- Authentification à deux facteurs : Renforcez la sécurité des comptes avec le 2FA
 4- Mises à jour : Assurez-vous que vos logiciels sont à jour
 5- Prudence : Employez un antivol physique et soyez vigilant contre l ingénierie sociale
+
 '
 "@
 
